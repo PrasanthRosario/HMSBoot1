aeaa0d46ff821b0025d5d9dd21c94b950881b044
PrasanthRosario <54268724+PrasanthRosario@users.noreply.github.com>
2020-01-20 18:15:08 +0530

    flyway migration updated

--- src/main/java/global/coda/hms/constants/DoctorQuery.java
+++ src/main/java/global/coda/hms/constants/DoctorQuery.java
@@ -1,6 +1,8 @@
 package global.coda.hms.constants;
+
 /**
  * Doctor Query Constants.
+ *
  * @author VC
  *
  */
@@ -11,6 +13,7 @@ public final class DoctorQuery {
 	private DoctorQuery() {
 
 	}
+
 	public static final String INSERT_DOCTOR = "INSERT INTO t_doctor (doctor_specialization, fk_user_id) VALUES (#{doctorSpecialization}, #{pkUserId})";
 
 	public static final String UPDATE_DOCTOR = "UPDATE t_doctor SET doctor_specialization = #{doctorSpecialization} WHERE (fk_user_id = #{pkUserId})";
@@ -23,14 +26,12 @@ public final class DoctorQuery {
 	public static final String READ_PATIENTS = "select patient.username,patient.firstname,record.fk_patient_id,patient.user_id, patient.lastname,patient.blood_group,patient.weight,patient.city,patient.state,patient.phone_number from t_record as record  "
 			+ "LEFT join (select user.pk_user_id as user_id, user.username,user.password,user.firstname,user.lastname,patient.blood_group,patient.weight,user.fk_role_id,user.city,user.state,user.created_time,user.updated_time, patient.pk_patient_id as patient_id,user.phone_number "
 			+ "from  t_user_details as user LEFT JOIN t_patient as patient ON user.pk_user_id = patient.fk_user_id where  patient.is_active = 1 "
-			+ "and fk_role_id = 3) as patient "
-			+ "on patient.patient_id = fk_patient_id "
+			+ "and fk_role_id = 3) as patient " + "on patient.patient_id = fk_patient_id "
 			+ "where record.fk_doctor_id = #{fkDoctorId} and record.is_active = 1 group by patient.patient_id";
 
 	public static final String DOCTOR_ID = "select distinct fk_doctor_id from t_record";
 
 	public static final String READ_ALL_DOCTORS = "<script> select * from hmsdb.t_user_details as user "
 			+ "LEFT JOIN hmsdb.t_doctor as doctor ON user.pk_user_id = doctor.fk_user_id where user.is_active = 1 and  user.fk_role_id = 4 <if test= 'pk_user_id !=0'> and pk_user_id = #{id}  </if></script>";
-
 
 }
--- src/main/java/global/coda/hms/controller/DoctorController.java
+++ src/main/java/global/coda/hms/controller/DoctorController.java
@@ -20,8 +20,10 @@ import global.coda.hms.exception.SystemException;
 import global.coda.hms.model.Doctor;
 import global.coda.hms.model.ResponseBody;
 import global.coda.hms.service.DoctorService;
+
 /**
  * Doctor Controller.
+ *
  * @author VC
  *
  */
@@ -29,21 +31,25 @@ import global.coda.hms.service.DoctorService;
 @RequestMapping("/doctors")
 public class DoctorController {
 
-	private final  Logger logger = LogManager.getLogger(DoctorController.class);
+	private final Logger logger = LogManager.getLogger(DoctorController.class);
 	private DoctorService doctorService;
+
 	/**
 	 * Constructor Method.
+	 *
 	 * @param doctorService doctor service
 	 */
 	public DoctorController(DoctorService doctorService) {
 		this.doctorService = doctorService;
 	}
+
 	/**
 	 * To read doctor.
+	 *
 	 * @param id user id
 	 * @return response response
 	 * @throws BusinessException business exception
-	 * @throws SystemException system exception
+	 * @throws SystemException   system exception
 	 */
 	@GetMapping("/{id}")
 	public ResponseBody<Doctor> getById(@PathVariable("id") String id) throws BusinessException, SystemException {
@@ -55,12 +61,14 @@ public class DoctorController {
 		return response;
 
 	}
+
 	/**
 	 * To create doctor.
+	 *
 	 * @param doctor doctor
 	 * @return response response
 	 * @throws BusinessException business exception
-	 * @throws SystemException system exception
+	 * @throws SystemException   system exception
 	 */
 	@PostMapping("/create")
 	public ResponseBody<Doctor> createDoctor(@RequestBody Doctor doctor) throws BusinessException, SystemException {
@@ -71,12 +79,14 @@ public class DoctorController {
 		logger.traceExit(response);
 		return response;
 	}
+
 	/**
 	 * To update Doctor.
+	 *
 	 * @param doctor doctor
 	 * @return response response
 	 * @throws BusinessException business exception
-	 * @throws SystemException system exception
+	 * @throws SystemException   system exception
 	 */
 	@PutMapping("/update")
 	public ResponseBody<Doctor> updateDoctor(@RequestBody Doctor doctor) throws BusinessException, SystemException {
@@ -87,12 +97,14 @@ public class DoctorController {
 		logger.traceExit(response);
 		return response;
 	}
+
 	/**
 	 * To delete the doctor.
+	 *
 	 * @param id user id
 	 * @return response response
 	 * @throws BusinessException business exception
-	 * @throws SystemException system exception
+	 * @throws SystemException   system exception
 	 */
 	@DeleteMapping("/delete/{id}")
 	public ResponseBody<String> delete(@PathVariable("id") String id) throws BusinessException, SystemException {
@@ -103,15 +115,18 @@ public class DoctorController {
 		logger.traceExit(response);
 		return response;
 	}
+
 	/**
 	 * read patient under a doctor.
+	 *
 	 * @param doctorId doctor id
 	 * @return resposne response
 	 * @throws BusinessException business exception
-	 * @throws SystemException system exception
+	 * @throws SystemException   system exception
 	 */
 	@GetMapping("/getPatients")
-	public ResponseBody<List<Doctor>> readAllPatients(@RequestParam(name = "userId" , required = false) String userId) throws BusinessException, SystemException {
+	public ResponseBody<List<Doctor>> readAllPatients(@RequestParam(name = "userId", required = false) String userId)
+			throws BusinessException, SystemException {
 		logger.entry(userId);
 		if (userId == null) {
 			userId = "0";
@@ -122,19 +137,20 @@ public class DoctorController {
 		logger.traceExit(response);
 		return response;
 	}
-	//	/**
-	//	 * To get all doctor and all patients.
-	//	 * @return response response
-	//	 * @throws BusinessException business exception
-	//	 * @throws SystemException system exception
-	//	 */
-	//	@GetMapping("/getPatients")
-	//	public ResponseBody<List<Doctor>> readAllUsers() throws BusinessException, SystemException {
-	//		logger.traceEntry();
-	//		ResponseBody<List<Doctor>> response = new ResponseBody<List<Doctor>>();
-	//		response.setSetStatus(ApplicationConstants.SUCCESS);
-	//		response.setSetMessage(doctorService.readAllDoctor("0"));
-	//		logger.traceExit(response);
-	//		return response;
-	//	}
+	// /**
+	// * To get all doctor and all patients.
+	// * @return response response
+	// * @throws BusinessException business exception
+	// * @throws SystemException system exception
+	// */
+	// @GetMapping("/getPatients")
+	// public ResponseBody<List<Doctor>> readAllUsers() throws BusinessException,
+	// SystemException {
+	// logger.traceEntry();
+	// ResponseBody<List<Doctor>> response = new ResponseBody<List<Doctor>>();
+	// response.setSetStatus(ApplicationConstants.SUCCESS);
+	// response.setSetMessage(doctorService.readAllDoctor("0"));
+	// logger.traceExit(response);
+	// return response;
+	// }
 }
