89eb643ad87e62a8d8b21cbfa6d385d3d3cea31a
PrasanthRosario <54268724+PrasanthRosario@users.noreply.github.com>
2020-01-20 15:05:33 +0530

    Sample changes 2

--- src/main/java/global/coda/hms/service/DoctorService.java
+++ src/main/java/global/coda/hms/service/DoctorService.java
@@ -57,7 +57,7 @@ public class DoctorService {
 		return doctor;
 	}
 	/**
-	 * To create doctor in the DB.
+	 * To create doctor in the mapper.
 	 * @param doctor doctor
 	 * @return doctor doctor
 	 * @throws BusinessException business exception
